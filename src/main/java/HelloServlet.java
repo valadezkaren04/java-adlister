58c3fd351c77bb83485331be8690d7100d5593c9
Karen Valadez <valadez.karen04@gmail.com>
2021-07-01 13:44:59 -0500

    lec notes in progress

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name"); // sets the value
+        req.setAttribute("name", name); // passed on into the view ; sets up the attribute
+        req.getRequestDispatcher("/hello.jsp").forward(req, resp); // fwrd to a jsp ; served by the servlet ; has the attribute replaced
+
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: alfredovaladez
+  Date: 7/1/21
+  Time: 11:54 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+    <h1>Hello, ${name}</h1>
+
+<%--Redeploy vs Restart--%>
+
+<%--Redeploy: reset the static content in our app. Updates made will redeploy ; refresh the assets --%>
+<%--Restarts: restarts the servlet itself ; safests update ; if made changes in Java folder--%>
+</body>
+</html>
