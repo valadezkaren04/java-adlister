1d978ea297c31e1ec632acb9e31a9f7982e5c8c0
Karen Valadez <valadez.karen04@gmail.com>
2021-07-08 09:13:11 -0500

    lec notes

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,26 +3,62 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/login").forward(req, resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("isAdmin") != null) {
+            response.sendRedirect("/secret-admin-page.jsp");
+            return;
+        }
+
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        boolean isAdmin = username.equals("admin") && password.equals("password");
 
-            String username = req.getParameter("username");
-            String password = req.getParameter("password");
+        HttpSession session = request.getSession();
 
-            if (username.equals("admin") && password.equals("password")) {
-                resp.sendRedirect("/profile");
-            } else {
-                resp.sendRedirect("/login");
+        if (isAdmin) {
+            session.setAttribute("isAdmin", true);
+            response.sendRedirect("/secret-admin-page");
+        } else {
+            response.sendRedirect("/login");
         }
+
+
+
+
+
+
+
+
+
+//        // set the session object to a variable based on the HttpSession class.
+//        HttpSession session = request.getSession();
+//
+//        String darkMode = request.getParameter("color");
+//
+//        // set the dark theme attribute on the session so that it gets remembered every time we log in.
+//        session.setAttribute("darkMode", darkMode);
+//
+//        // on a different servlet (profile page).
+//        // with our getAttribute method, our return is always a generic Object type.
+//        // therefore we need to explicitly cast the object to the correct type.
+//        session.getAttribute("darkMode");
+//        String darkSelected = (String) session.getAttribute("darkMode");
+//
+//        // to remove an attribute
+//        session.removeAttribute("darkMode");
+//
+//        // destroys the session
+//        session.invalidate();
+
 
     }
 }
---
+++ src/main/webapp/secret-admin-page.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: alfredovaladez
+  Date: 7/8/21
+  Time: 9:10 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Secret Admin Page</title>
+</head>
+<body>
+<h1>Shhhh it's a secret!</h1>
+</body>
+</html>
